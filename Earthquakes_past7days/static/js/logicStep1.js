3d08876cd53278d5206993524d93e79917cafd2b
Michaelangelo <Michaelangelo@Gelos-MacBook-Pro.local>
2022-05-21 20:16:30 -0400

    Adding Earthquake Data

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,5 +1,5 @@
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -20,24 +20,17 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let torontoHoods = 'https://raw.githubusercontent.com/michaelangelo-kodjoe/mapping_earthquakes/main/torontoNeighborhoods.json';
 
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
-
-d3.json(torontoHoods).then(function(data){
+// Retrieve the earthquake GeoJSON data.
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(function(data){
   console.log(data);
   L.geoJSON(data,{
     onEachFeature: function(feature, layer) {
@@ -45,5 +38,4 @@ d3.json(torontoHoods).then(function(data){
       layer.bindPopup('<h3>' + 'Neighborhood: ' + feature.properties.AREA_NAME + '</h3>')
       
     },
-      style: myStyle
   }).addTo(map);});
