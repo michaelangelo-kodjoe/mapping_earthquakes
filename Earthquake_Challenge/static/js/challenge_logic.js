bcdece10d9330c56e478cea5300ead7ff69ae960
Michaelangelo <Michaelangelo@Gelos-MacBook-Pro.local>
2022-05-22 14:56:59 -0400

    Solving a challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -170,7 +170,7 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   }
   }).addTo(majorEQ);
 
-  //  Add the major earthquakes layer to the map.
+  //  we add the major earthquakes layer to the map.
   majorEQ.addTo(map)
 //  Close the braces and parentheses for the major earthquake data.
 });
